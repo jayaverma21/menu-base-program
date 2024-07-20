f962a847f3f2406a7bfb14dd231db9044b86437f
jaya verma <151820500+jayaverma21@users.noreply.github.com>
2024-07-20 12:22:44 +0530

    Create sms.py

---
+++ Cgi-bin/sms.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import cgi
+import cgitb
+from twilio.rest import Client
+
+# Enable debugging
+cgitb.enable()
+
+# Twilio credentials
+account_sid = 'Enter your SID'
+auth_token = 'Enter your auth_tocken'
+
+# Function to send SMS
+def send_sms():
+    form = cgi.FieldStorage()
+    to_number = form.getvalue('to')
+    message_body = form.getvalue('body')
+
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body=message_body,
+        to=to_number,  # Receiver mobile number
+        from_='twilio number'  # Provided by Twilio API
+    )
+    return message.sid
+
+print("Content-Type: text/html")
+print()
+print(send_sms())
