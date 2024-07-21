d3f5ca2268546d17dd057426c4532378c465e852
jaya verma <151820500+jayaverma21@users.noreply.github.com>
2024-07-21 22:09:06 +0530

    Create Docker.py

---
+++ Cgi-bin/Docker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import subprocess
+import cgi
+
+form = cgi.FieldStorage()
+cmd = form.getvalue("cmd")
+
+print("Content-type:text/html")
+print("")
+
+finalCommand = "sudo "+cmd
+output = subprocess.getoutput(finalCommand)
+print("<pre>")
+print(output)
